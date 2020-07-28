dba2464cda22e9a04b6d969f4ad3ece0969634ad
Adolfo <yml.aldana@gmail.com>
2020-07-27 21:23:10 -0400

    obteniendo letra con axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
+import Axios from 'axios';
+
 
 function App() {
 
   const [buscarletra, setbuscarletra] = useState({})
+  const [letra, guardarLetra] = useState('')
 
   useEffect(() => {
     if (Object.keys(buscarletra).length === 0) return;
@@ -11,8 +14,11 @@ function App() {
 
     const consultandoApiLetra = async () => {
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
-    }
+      const respuesta = await Axios.get(url);
 
+      guardarLetra(respuesta.data.lyrics);
+    }
+    consultandoApiLetra();
   }, [buscarletra])
 
   return (
