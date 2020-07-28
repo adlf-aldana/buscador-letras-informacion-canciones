ad5f7ea5557274b484e4e272f63b998958baf7db
Adolfo <yml.aldana@gmail.com>
2020-07-27 21:52:59 -0400

    obteniendo informacion del artista

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 
   const [buscarletra, setbuscarletra] = useState({})
   const [letra, guardarLetra] = useState('')
+  const [info, guardarInfo] = useState({})
 
   useEffect(() => {
     if (Object.keys(buscarletra).length === 0) return;
@@ -16,9 +17,18 @@ function App() {
 
     const consultandoApiLetra = async () => {
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
-      const respuesta = await Axios.get(url);
+      const url2 = ` theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
 
-      guardarLetra(respuesta.data.lyrics);
+      const [letra, informacion] = await Promise.all([
+        Axios.get(url),
+        Axios.get(url2)
+      ])
+      // const respuesta = await Axios.get(url);
+      // const respuesta2 = await Axios.get(url2);
+
+      // guardarLetra(respuesta.data.lyrics);
+      guardarLetra(letra.data.lyrics);
+      guardarInfo(informacion.data.artista[0])
     }
     consultandoApiLetra();
   }, [buscarletra])
@@ -30,7 +40,7 @@ function App() {
       <div className="container m-5">
         <div className="row">
           <div className="col-md-6">
-            
+
           </div>
           <div className="col-md-6">
             <Letra letra={letra} />
