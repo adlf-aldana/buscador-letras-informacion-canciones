86dd8b57788750ee57173f779461f585d21d3933
Adolfo <yml.aldana@gmail.com>
2020-07-27 22:19:17 -0400

    creado componente info, y mostrando datos del artista

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 import Axios from 'axios';
 import Letra from './components/Letra';
+import Info from './components/Info';
 
 
 
@@ -17,7 +18,7 @@ function App() {
 
     const consultandoApiLetra = async () => {
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
-      const url2 = ` theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
 
       const [letra, informacion] = await Promise.all([
         Axios.get(url),
@@ -28,10 +29,11 @@ function App() {
 
       // guardarLetra(respuesta.data.lyrics);
       guardarLetra(letra.data.lyrics);
-      guardarInfo(informacion.data.artista[0])
+      guardarInfo(informacion.data.artists[0])
+      
     }
     consultandoApiLetra();
-  }, [buscarletra])
+  }, [buscarletra, info])
 
   return (
     <Fragment>
@@ -40,7 +42,7 @@ function App() {
       <div className="container m-5">
         <div className="row">
           <div className="col-md-6">
-
+            <Info info={info} />
           </div>
           <div className="col-md-6">
             <Letra letra={letra} />
